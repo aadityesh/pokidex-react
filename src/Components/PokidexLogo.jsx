25a7ecc1995604614f68b066c3e69c230c6eaa28
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-11-05 20:50:46 +0530

    Update PokidexLogo.jsx

--- src/Components/PokidexLogo.jsx
+++ src/Components/PokidexLogo.jsx
@@ -4,7 +4,7 @@ function PokidexLogo() {
   return (
     <div className="flex justify-between items-center">
       <img src="/assets/icons/Pokedex-logo.svg" alt="pokedex-logo" />
-      <a href="">
+      <a href="https://github.com/aadityesh/pokidex-react">
         <img src="/assets/icons/github.svg" alt="github" />
       </a>
     </div>
